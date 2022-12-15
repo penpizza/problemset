ef8e30d3c45dcca2d0d61a01f45fae8368d27c2a
penpizza <110964198+penpizza@users.noreply.github.com>
2022-12-15 16:38:29 +0700

    Add files via upload
    โปรแกรมหาพื้นที่สามเหลี่ยม

---
+++ PB1_5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double height,base,area;
+    cout << "Enter height : ";
+    cin >> height;
+    cout << "Enter base : ";
+    cin >> base;
+    area = 0.5*base*height; 
+    cout << "Area = "<<area ;
+    
+}
